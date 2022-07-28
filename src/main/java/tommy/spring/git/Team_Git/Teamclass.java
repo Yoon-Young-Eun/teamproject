6c44da9058f5dc50ce5de60876e5a72f6f0a933e
YongHyunBaek <slayers_yh@nate.com>
2022-07-28 20:38:07 +0900

    a

--- src/main/java/tommy/spring/git/Team_Git/Teamclass.java
+++ src/main/java/tommy/spring/git/Team_Git/Teamclass.java
@@ -10,6 +10,9 @@ System.out.println("깃허브!!! 이!#@$%#@%^!#$Q %@");
 System.out.println("깃허브!!! 이!#@$%#@%^!#$Q %@");
 System.out.println("깃허브!!! 이!#@$%#@%^!#$Q %@");
 System.out.println("깃허브!!! 이!#@$%#@%^!#$Q %@");
+System.out.println("집에가고싶다 ");
+System.out.println("이거 확인되면 집에 가고 싶다.");
+
 	}
 
 }

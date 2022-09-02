4b4341a6feb691768f9aac51e9ce91ce77d2b442
kimyealin <ksv3232@naver.com>
2022-09-02 14:48:35 +0900

    ddd

--- src/main/java/com/semo/web/admin/controller/Ad_BoardController.java
+++ src/main/java/com/semo/web/admin/controller/Ad_BoardController.java
@@ -1,8 +1,52 @@
 package com.semo.web.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.semo.web.admin.service.BoardService;
+import com.semo.web.admin.vo.NoticeVO;
 
 @Controller
 public class Ad_BoardController {
-// 계시판 관련(Q&A, BOARD, 공지사항, 자주하는 질문)
+
+	@Autowired
+	private BoardService boardservice;
+
+	@RequestMapping("/getBoardList.mdo")
+	public String getBoardList(NoticeVO vo, Model model) {
+		System.out.println("글 목록 처리");
+		model.addAttribute("boardList", boardservice.getBoardList(vo));
+		return "board_notice.jsp";
+	}
+
+	@RequestMapping("/getBoard.mdo")
+	public String getBoard(NoticeVO vo, Model model) {
+		System.out.println("글 상세 보기 처리");
+		model.addAttribute("board", boardservice.getBoard(vo));
+		return "getBoard.jsp";
+	}
+
+	@RequestMapping("/insertBoard.mdo")
+	public String insertBoard(NoticeVO vo) {
+		System.out.println("글 등록 처리");
+		boardservice.insertBoard(vo);
+		return "getBoardList.mdo";
+	}
+
+	@RequestMapping("/updateBoard.mdo")
+	public String updateBoard(NoticeVO vo) {
+		System.out.println("글 수정 기능 처리");
+		boardservice.updateBoard(vo);
+		return "getBoardList.mdo";
+	}
+
+	@RequestMapping("/deleteBoard.mdo")
+	public String deleteBoard(NoticeVO vo) {
+		System.out.println("글 삭제 처리");
+		boardservice.deleteBoard(vo);
+		return "getBoardList.mdo";
+	}
+
 }
--- src/main/java/com/semo/web/admin/dao/BoardDAO.java
+++ src/main/java/com/semo/web/admin/dao/BoardDAO.java
@@ -1,8 +1,47 @@
 package com.semo.web.admin.dao;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.semo.web.admin.vo.NoticeVO;
 
 @Repository
 public class BoardDAO {
+
+	public BoardDAO() {
+
+	}
+
+	@Autowired
+	SqlSessionTemplate sqltemplate;
+
+
+	public void insertBoard(NoticeVO vo) {
+		sqltemplate.insert("BoardDAO.insertBoard",vo);
+
+	}
+
+	public void updateBoard(NoticeVO vo) {
+		sqltemplate.update("BoardDAO.updateBoard",vo);
+
+	}
+
+	public void deleteBoard(NoticeVO vo) {
+		sqltemplate.delete("BoardDAO.deleteBoard",vo);
+
+	}
+
+
+	public NoticeVO getBoard(NoticeVO vo) {
+		return sqltemplate.selectOne("BoardDAO.getBoard",vo);
+	}
+
+	public List<NoticeVO> getBoardList(NoticeVO vo) {
+		return sqltemplate.selectList("BoardDAO.getBoardList",vo);
+	}
+
 
 }

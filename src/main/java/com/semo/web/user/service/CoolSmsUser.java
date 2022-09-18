e815b0879652c71fe8b39920dbc6e9e0ba0219ab
kimyealin <ksv3232@naver.com>
2022-09-18 16:42:59 +0900

    쿠울유저

--- src/main/java/com/semo/web/user/service/CoolSmsUser.java
+++ src/main/java/com/semo/web/user/service/CoolSmsUser.java
@@ -3,7 +3,6 @@ package com.semo.web.user.service;
 import java.util.HashMap;
 
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.json.simple.JSONObject;
 
 import net.nurigo.java_sdk.api.Message;

ed8324315a0b596f30fd22afe2766044ee0967f2
lee <leegh1011@gmail.com>
2022-09-25 18:17:24 +0900

    문의하기 수정 중

--- src/main/java/com/semo/web/user/controller/MypageController.java
+++ src/main/java/com/semo/web/user/controller/MypageController.java
@@ -496,9 +496,6 @@ public class MypageController {
     	return "/views-mypage/MyReview.jsp";
     }
     
-<<<<<<< HEAD
-   
-=======
     //리뷰 상세보기
     @RequestMapping(value="/viewReview.do")
     public String viewReview(ReviewVO vo,Model model) {
@@ -508,6 +505,5 @@ public class MypageController {
     	System.out.println(service.viewReview(vo));
     	return "/views-mypage/myReviewView.jsp";
     }
->>>>>>> main
 		
 }
